12188f47e31cc33c6b7b403920e2e69641b903c1
MeghlaB <meghlab964@gmail.com>
2025-07-10 18:38:20 +0600

    added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import BookingCarousel from '../BookingCarousel/BookingCarousel'
 import GoLimitlessBanner from '../GoLimitlessBanner'
+import HotDeals from '../HotDeals'
+import StudentOfferBanner from '../StudentOfferBanner'
 
 function Home() {
   return (
     <div>
       <BookingCarousel></BookingCarousel>
       <GoLimitlessBanner/>
+      <HotDeals/>
+      <StudentOfferBanner/>
     </div>
   )
 }
